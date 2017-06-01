ce09f65c4092429e9b341fbff20ceb10b34e7791
Ana <analu.610@gmail.com>
2017-06-01 10:37:18 +0200

    First version of project: needs improved perception/decision making

--- code/decision.py
+++ code/decision.py
@@ -10,12 +10,27 @@ def decision_step(Rover):
     # improve on this decision tree to do a good job of navigating autonomously!
 
     # Example:
+    min_ahead = 20 ###*
+    #Rover.stop_forward = 200 ###*
+    
+    
+    
     # Check if we have vision data to make decisions with
     if Rover.nav_angles is not None:
+        
+       # Number of navigable pixels in steering direction
+        angles_list = list(Rover.nav_angles) ###*
+        navigable_indir = angles_list.count(0) ###*
+        
+        # Number of navigable, continuos pixels in steering direction
+        
+        
         # Check for Rover.mode status
         if Rover.mode == 'forward': 
+            
             # Check the extent of navigable terrain
-            if len(Rover.nav_angles) >= Rover.stop_forward:  
+            # If there is enough navigable terrain and no obstacles ahead
+            if len(Rover.nav_angles) >= Rover.stop_forward and navigable_indir >= min_ahead:  ###*
                 # If mode is forward, navigable terrain looks good 
                 # and velocity is below max, then throttle 
                 if Rover.vel < Rover.max_vel:
@@ -27,7 +42,7 @@ def decision_step(Rover):
                 # Set steering to average angle clipped to the range +/- 15
                 Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
             # If there's a lack of navigable terrain pixels then go to 'stop' mode
-            elif len(Rover.nav_angles) < Rover.stop_forward:
+            elif len(Rover.nav_angles) < Rover.stop_forward  or navigable_indir < min_ahead: ###*
                     # Set mode to "stop" and hit the brakes!
                     Rover.throttle = 0
                     # Set brake to stored brake value
@@ -45,14 +60,14 @@ def decision_step(Rover):
             # If we're not moving (vel < 0.2) then do something else
             elif Rover.vel <= 0.2:
                 # Now we're stopped and we have vision data to see if there's a path forward
-                if len(Rover.nav_angles) < Rover.go_forward:
+                if len(Rover.nav_angles) < Rover.go_forward or navigable_indir < min_ahead: ###*
                     Rover.throttle = 0
                     # Release the brake to allow turning
                     Rover.brake = 0
                     # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
-                    Rover.steer = -15 # Could be more clever here about which way to turn
+                    Rover.steer = -15 # Could be more clever here about which way to turn*****
                 # If we're stopped but see sufficient navigable terrain in front then go!
-                if len(Rover.nav_angles) >= Rover.go_forward:
+                if len(Rover.nav_angles) >= Rover.go_forward and navigable_indir >= min_ahead: ###*
                     # Set throttle back to stored value
                     Rover.throttle = Rover.throttle_set
                     # Release the brake
--- code/perception.py
+++ code/perception.py
@@ -1,11 +1,14 @@
 import numpy as np
 import cv2
 
-# Identify pixels above the threshold
-# Threshold of RGB > 160 does a nice job of identifying ground pixels only
-def color_thresh(img, rgb_thresh=(160, 160, 160)):
+
+# Detection (thresholding)
+def color_thresh(img, rgb_thresh=(160, 160, 160),rgb_lowthresh=(8, 5, 3), b_thresh=(165,255)):
+    
+    # ----> Navigable terrain  
     # Create an array of zeros same xy size as img, but single channel
-    color_select = np.zeros_like(img[:,:,0])
+    navigable_select = np.zeros_like(img[:,:,0])
+
     # Require that each pixel be above all three threshold values in RGB
     # above_thresh will now contain a boolean array with "True"
     # where threshold was met
@@ -13,9 +16,34 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
                 & (img[:,:,1] > rgb_thresh[1]) \
                 & (img[:,:,2] > rgb_thresh[2])
     # Index the array of zeros with the boolean array and set to 1
-    color_select[above_thresh] = 1
-    # Return the binary image
-    return color_select
+    navigable_select[above_thresh] = 1
+    
+    
+    # ----> Obstacles
+    obstacle_select = np.zeros_like(img[:,:,0])
+    below_thresh = (img[:,:,0] < 76) \
+                & (img[:,:,1] < 60) \
+                & (img[:,:,2] < 28) \
+                &(img[:,:,0] > rgb_lowthresh[0]) \
+                & (img[:,:,1] > rgb_lowthresh[1]) \
+                & (img[:,:,2] > rgb_lowthresh[2])
+    obstacle_select[below_thresh] = 1
+    
+    
+    # ----> Rock sample threshold
+    # convert from RGB to LAB
+    lab = cv2.cvtColor(img, cv2.COLOR_RGB2Lab) 
+    # take b_channel (yellow)
+    b_channel = lab[:,:,2] 
+    # create binary image same size as original image
+    rock_binary = np.zeros_like(b_channel)
+    # set pixels containing yellow to 1
+    rock_binary[(b_channel >= b_thresh[0]) & (b_channel <=  b_thresh[1])] = 1
+
+
+    # Return the binary images      
+    return navigable_select,obstacle_select,rock_binary
+
 
 # Define a function to convert to rover-centric coordinates
 def rover_coords(binary_img):
@@ -42,9 +70,10 @@ def to_polar_coords(x_pixel, y_pixel):
 def rotate_pix(xpix, ypix, yaw):
     # TODO:
     # Convert yaw to radians
+    yaw_rad = yaw * (np.pi/180)
     # Apply a rotation
-    xpix_rotated = 0
-    ypix_rotated = 0
+    xpix_rotated = xpix * np.cos(yaw_rad) - ypix * np.sin(yaw_rad)
+    ypix_rotated = xpix * np.sin(yaw_rad) + ypix * np.cos(yaw_rad)
     # Return the result  
     return xpix_rotated, ypix_rotated
 
@@ -52,10 +81,11 @@ def rotate_pix(xpix, ypix, yaw):
 def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
     # TODO:
     # Apply a scaling and a translation
-    xpix_translated = 0
-    ypix_translated = 0
+    xpix_translated = xpos + (xpix_rot/scale)
+    ypix_translated = ypos + (ypix_rot/scale)
     # Return the result  
     return xpix_translated, ypix_translated
+
 
 # Define a function to apply rotation and translation (and clipping)
 # Once you define the two functions above this function should work
@@ -84,25 +114,66 @@ def perception_step(Rover):
     # Perform perception steps to update Rover()
     # TODO: 
     # NOTE: camera image is coming to you in Rover.img
+    img = Rover.img
+
     # 1) Define source and destination points for perspective transform
+    dst_size = 5 
+    # rover position is not bottom of image, but a bit offset
+    bottom_offset = 6 
+    source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
+    destination = np.float32([[img.shape[1]/2 - dst_size, img.shape[0] - bottom_offset],
+                      [img.shape[1]/2 + dst_size, img.shape[0] - bottom_offset],
+                      [img.shape[1]/2 + dst_size, img.shape[0] - 2*dst_size - bottom_offset], 
+                      [img.shape[1]/2 - dst_size, img.shape[0] - 2*dst_size - bottom_offset],
+                      ])
+
     # 2) Apply perspective transform
+    warped = perspect_transform(img, source, destination)
+
     # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
+    navigable, obstacles, rocks = color_thresh(warped)
+
     # 4) Update Rover.vision_image (this will be displayed on left side of screen)
         # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
         #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
         #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
 
+    Rover.vision_image[:,:,0] = obstacles*255
+    Rover.vision_image[:,:,1] = rocks*255
+    Rover.vision_image[:,:,2] = navigable*255
+
     # 5) Convert map image pixel values to rover-centric coords
+    navigable_xpix, navigable_ypix = rover_coords(navigable)
+    obstacles_xpix, obstacles_ypix = rover_coords(obstacles)
+    rocks_xpix, rocks_ypix = rover_coords(rocks)
+
     # 6) Convert rover-centric pixel values to world coordinates
+    world_size = Rover.worldmap.shape[0];
+    scale = 10
+    xpos = Rover.pos[0]
+    ypos = Rover.pos[1]
+    yaw = Rover.yaw
+    navigable_x_world, navigable_y_world = pix_to_world(navigable_xpix, navigable_ypix, xpos, ypos, yaw, world_size, scale)
+    obstacles_x_world, obstacles_y_world = pix_to_world(obstacles_xpix, obstacles_ypix, xpos, ypos, yaw, world_size, scale)
+    rocks_x_world, rocks_y_world = pix_to_world(rocks_xpix, rocks_ypix, xpos, ypos, yaw, world_size, scale)
+
     # 7) Update Rover worldmap (to be displayed on right side of screen)
         # Example: Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
         #          Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
         #          Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+    Rover.worldmap[obstacles_y_world, obstacles_x_world, 0] += 1 
+    Rover.worldmap[rocks_y_world, rocks_x_world, 1] += 1 
+    Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1 
+
+
 
     # 8) Convert rover-centric pixel positions to polar coordinates
     # Update Rover pixel distances and angles
         # Rover.nav_dists = rover_centric_pixel_distances
         # Rover.nav_angles = rover_centric_angles
+    rover_centric_pixel_distances, rover_centric_angles = to_polar_coords(navigable_xpix, navigable_ypix)
+    Rover.nav_dists = rover_centric_pixel_distances
+    Rover.nav_angles = rover_centric_angles
     
  
     
